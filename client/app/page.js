1802bdb7653ca79da17904ab35e0710790b607a2
ajirang1 <hehasfelt@gmail.com>
2025-08-24 14:58:00 +0900

    Changed to school version, returns schoolName, foodData, timeData

--- client/app/page.js
+++ client/app/page.js
@@ -8,6 +8,9 @@ export default function Home() {
 
     const [message, setMessage] = useState('')
     const [username, setUsername] = useState('')
+    const [schoolName, setSchoolName] = useState('동패고등학교')
+    const [gradeNum, setGradeNum] = useState(2)
+    const [classNum, setClassNum] = useState(9)
 
     function handleSubmit(username) {
             fetch(`http://localhost:8080/api?username=${username}`)
@@ -18,25 +21,62 @@ export default function Home() {
     }
 
     function handleKeyDown(e) {
-        if (e.key === "Enter") {
+        if (e.key === "Enter" && schoolName && gradeNum && classNum) {
             handleSubmit(username)
+        } else {
+            setMessage('학교 이름이나 반, 번호를 제대로 선택했는지 확인해주세요!')
         }
+    }
+
+    function handleSchoolSubmit(schoolName, gradeNum, classNum) {
+        fetch(`http://localhost:8080/schoolinfo?schoolName=${schoolName}&gradeNum=${gradeNum}&classNum=${classNum}`)
+            .then((response) => {response.json().then((data) => {
+                console.log(data)
+                if (data.errmsg) {
+                    setMessage(data.errmsg)
+                } else {
+                    const messageSetting = JSON.stringify(data)
+                    setMessage(messageSetting)
+                }
+            })})
     }
 
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
-        <Input
-            className="w-100 mb-5 bg-white"
-            placeholder="Input Player Name"
-            type="text"
-            onChange={(e) => setUsername(e.target.value)}
-            value={username}
-            onKeyDown={handleKeyDown}
-        />
+        <div className="flex flex-row w-70 mb-4 gap-2">
+            <Input
+                className="bg-white w-200"
+                placeholder="학교 이름"
+                type="text"
+                name="schoolName"
+                onChange={(e) => setUsername(e.target.value)}
+                value={schoolName}
+                onKeyDown={handleKeyDown}
+            />
+            <Input
+                className="bg-white"
+                placeholder="학년"
+                type="text"
+                name="gradeNum"
+                onChange={(e) => setGradeNum(e.target.value)}
+                value={gradeNum}
+                onKeyDown={handleKeyDown}
+            />
+            <Input
+                className="bg-white"
+                placeholder="반"
+                type="text"
+                name="classNum"
+                onChange={(e) => setClassNum(e.target.value)}
+                value={classNum}
+                onKeyDown={handleKeyDown}
+            />
+        </div>
+
         <Button
             className="mb-5"
-        onClick={() => handleSubmit(username)}>Search!
+        onClick={() => handleSchoolSubmit(schoolName, gradeNum, classNum)}>제출!
         </Button>
         {message ? <p>{message}</p> : null}
     </div>
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const PORT = 8080
 const app = express()
+require('dotenv').config()
 
 app.use(cors())
 
@@ -20,6 +21,97 @@ app.get('/api', (req, res) => {
         })
     })
     console.log('Rank sent!')
+})
+
+app.get('/schoolinfo', (req, res) => {
+    const { schoolName, gradeNum, classNum } = req.query
+    let schoolCode;
+    let ofcdcCode;
+    let today = new Date();
+    let regulatedDay = today
+    let newMonth;
+    let foodData;
+    let timeData;
+
+    console.log({schoolName, gradeNum, classNum})
+
+    if (today.getDay() == 0) {
+        regulatedDay.setDate(today.getDate() + 1)
+    } else if (today.getDay() == 6) {
+        regulatedDay.setDate(today.getDate() + 2)
+    }
+
+    console.log(regulatedDay.getMonth().toString())
+
+    if (regulatedDay.getMonth().toString().length === 1) {
+        newMonth = `0`+ `${(regulatedDay.getMonth() + 1).toString()}`
+        console.log(newMonth)
+    }
+
+    console.log(`${regulatedDay.getFullYear()}${newMonth}${regulatedDay.getDate()}`)
+    const finDateYMD = `${regulatedDay.getFullYear()}${newMonth}${regulatedDay.getDate()}`
+
+    async function getFoodData() {
+        await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.NEIS_KEY}&Type=json&pIndex=1&pSize=1&ATPT_OFCDC_SC_CODE=${ofcdcCode}&SD_SCHUL_CODE=${schoolCode}&MLSV_YMD=${finDateYMD}`).then((response) => {
+            return response.json().then(async (data) => {
+                if(data.RESULT) {
+                    res.json({errmsg: data.RESULT.MESSAGE + `급식, res`})
+                } else {
+                    if(data.mealServiceDietInfo[0].head[1].RESULT.CODE === "INFO-000") {
+                        foodData = data.mealServiceDietInfo[1].row[0].DDISH_NM
+                        console.log(foodData)
+                    } else {
+                        res.json({errmsg: data.mealServiceDietInfo[0].head[1].RESULT.MESSAGE + `급식`, errcd: data.mealServiceDietInfo[0].head[1].RESULT.CODE})
+                    }
+                }
+            })
+        })
+    }
+
+    async function getTimeline() {
+        await fetch(`https://open.neis.go.kr/hub/hisTimetable?KEY=${process.env.NEIS_KEY}&Type=json&pIndex=1&pSize=7&ATPT_OFCDC_SC_CODE=${ofcdcCode}&SD_SCHUL_CODE=${schoolCode}&ALL_TI_YMD=${finDateYMD}&GRADE=${gradeNum}&CLASS_NM=${classNum}`).then((response) => {
+            return response.json().then(async (data) => {
+                if(data.RESULT) {
+                    res.json({errmsg: data.RESULT.MESSAGE + `시간표, res`})
+                } else {
+                    if(data.hisTimetable[0].head[1].RESULT.CODE === "INFO-000") {
+                        timeData = data.hisTimetable[1].row
+                        console.log(timeData)
+                    } else {
+                        res.json({errmsg: data.hisTimetable[0].head[1].RESULT.MESSAGE + `시간표`, errcd: data.hisTimetable[0].head[1].RESULT.CODE})
+                    }
+                }
+            })
+        })
+    }
+
+    fetch(`https://open.neis.go.kr/hub/schoolInfo?KEY=${process.env.NEIS_KEY}&Type=json&pIndex=1&pSize=5&SCHUL_NM=${schoolName}`).then((response) => {
+        response.json().then(async (data) => {
+            try {
+
+                if (data.RESULT) {
+                    res.json({errmsg: data.RESULT.MESSAGE + `반정보, res`})
+                } else {
+                    if (data.schoolInfo[0].head[1].RESULT.CODE === `INFO-000`) {
+                        schoolCode = data.schoolInfo[1].row[0].SD_SCHUL_CODE
+                        console.log(schoolCode)
+                        ofcdcCode = data.schoolInfo[1].row[0].ATPT_OFCDC_SC_CODE
+                        console.log(ofcdcCode)
+                        await getFoodData()
+                        await getTimeline()
+                        await res.status(200).json({schoolName, foodData, timeData})
+
+                    } else {
+                        res.json({errmsg: data.schoolInfo[0].head[1].RESULT.MESSAGE + `반정보`})
+                    }
+                }
+
+            } catch (err) {
+                console.error(err)
+            }
+            
+        })
+    })
 })
 
 app.listen(PORT, () => {
